e45e7b4cc18db17739026f5f0e641eb591eec149
Alex White (wu) <VVu@geekfarm.org>
2011-04-19 01:04:46 +0000

    Import of VVU/Proc-Launcher-0.0.34 from CPAN.
    gitpan-cpan-distribution: Proc-Launcher
    gitpan-cpan-version:      0.0.34
    gitpan-cpan-path:         VVU/Proc-Launcher-0.0.34.tar.gz
    gitpan-cpan-author:       VVU
    gitpan-cpan-maturity:     released

--- lib/Proc/Launcher.pm
+++ lib/Proc/Launcher.pm
@@ -2,7 +2,7 @@ package Proc::Launcher;
 use strict;
 use warnings;
 
-our $VERSION = '0.0.33'; # VERSION
+our $VERSION = '0.0.34'; # VERSION
 
 use Mouse;
 
@@ -21,7 +21,7 @@ Proc::Launcher - yet another forking process controller
 
 =head1 VERSION
 
-version 0.0.33
+version 0.0.34
 
 =head1 SYNOPSIS
 
